08c78b58c585127ca1562246ed24400358b952f5
Zainab Khan <zainabkhan@Zainabs-MacBook-Air.local>
2026-07-17 12:13:46 +0530

    api created

--- routers/cities.py
+++ routers/cities.py
@@ -1,12 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session, select
+from database import get_session
+from models.city import City, CityMetric
 router = APIRouter(prefix="/cities", tags=["Cities"])
 @router.get("/")
-def list_cities():
-    return {"message": "list all cities - TODO"}
+def list_cities(session: Session = Depends(get_session)):
+    return session.exec(select(City)).all()
+
  
 @router.get("/{city_id}")
-def get_city(city_id: int):
-    return {"message": f"get city {city_id} - TODO"}
+def get_city(city_id: int, session: Session= Depends(get_session)):
+    city = session.get(City, city_id)
+    if not city:
+        raise HTTPException(status_code=404, detail="City nahi mili")
+    return city
  
 @router.post("/")
 def add_city():
@@ -17,5 +24,6 @@ def update_city(city_id: int):
     return {"message": f"update city {city_id} - TODO"}
  
 @router.get("/{city_id}/history")
-def city_history(city_id: int):
-    return {"message": f"metrics history for city {city_id}"}
+def city_history(city_id: int, session: Session = Depends(get_session)):
+    statement = select(CityMetric).where(CityMetric.city_id == city_id)
+    return session.exec(statement).all()
